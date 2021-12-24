80fb0285492f07e2378830f234ed48af46822fb4
jayeon <jayeon.yoo@snu.ac.kr>
2021-12-25 01:22:59 +0900

    Sim10k  configs

--- scripts/train_ga_ca_vgg_sim10k.sh
+++ scripts/train_ga_ca_vgg_sim10k.sh
@@ -1,5 +1,6 @@
 python -m torch.distributed.launch \
-    --nproc_per_node=2 \
+    --nproc_per_node=1 \
     --master_port=$((RANDOM + 10000)) \
     tools/train_net_da.py \
     --config-file ./configs/da_ga_ca_sim10k_VGG_16_FPN_4x.yaml \
+    OUTPUT_DIR ./training_dir/TMP
